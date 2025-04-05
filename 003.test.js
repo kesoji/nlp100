59f7cc07d231016503b93ca4bf08875bbff16062
kesoji <6465251+kesoji@users.noreply.github.com>
2025-04-05 17:20:53 +0900

    003

---
+++ 003.test.js
@@ -0,0 +1,21 @@
+import { test, expect } from "vitest";
+
+// https://nlp100.github.io/ja/ch01.html#03-円周率
+// 003. 円周率
+// "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+// という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+test("test", () => {
+  expect(answer("Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."))
+  .toEqual([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9])
+});
+
+function answer(sentence) {
+  const words = sentence.split(/[\s+,.]/).filter(word => word.length > 0);
+  const ans = [];
+  for (const word of words) {
+    ans.push(word.length);
+  }
+
+  return ans;
+}
