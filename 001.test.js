9fc68a8cbe076a9dbe498a1fc6c68fe97b378054
kesoji <6465251+kesoji@users.noreply.github.com>
2025-04-05 17:23:41 +0900

    refactor: 問題文をimportより上に

--- 001.test.js
+++ 001.test.js
@@ -1,8 +1,8 @@
-import { test, expect } from "vitest";
-
 // https://nlp100.github.io/ja/ch01.html#01-%E3%83%91%E3%82%BF%E3%83%88%E3%82%AF%E3%82%AB%E3%82%B7%E3%83%BC%E3%83%BC
 // 01. 「パタトクカシーー」
 // 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
+
+import { test, expect } from "vitest";
 
 test("test", () => {
   expect(answer("パタトクカシーー")).toBe("パトカー");
